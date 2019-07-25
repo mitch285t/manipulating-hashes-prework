c6a27b958e7ffc1b39dcbed8d8da598cb0574f3f
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-25 20:13:00 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,7 @@ contact.each do |person, data|
   data.each do |attribue, value|
     if attribute == 
       :favorite_icecream_flavors
-    value.delete if do |flavor|
+    value.delete_if do |flavor|
       flavor == "Strawberry"
     
 
